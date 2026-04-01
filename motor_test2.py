dc85961db96a0762b87a4f41ffe273bf102aa232
Schlauer Fuchs <valentin.wittmer@gmail.com>
2026-04-01 15:25:06 +0200

    valis tast für den roboter

--- motor_test2.py
+++ motor_test2.py
@@ -1,27 +1,19 @@
-from buildhat import Motor
-from buildhat import MotorPair
+from buildhat import Motor, ColorDistanceSensor, MotorPair
 import time
 
-print("int motor A")
-lift = Motor('A')
-print("int motor B")
-zange = Motor('B')
-print("int motor C")
-ml = Motor('C')
-print("int motor D")
-mr = Motor('D')
 
-#fahren = MotorPair('C', 'D')
-#fahren.set_default_speed(50)
-#fahren.run_for_rotations(2)
-#fahren.run_for_rotations(1, speedl=100, speedr=50)
-print("Running lift for -90 and zange for 90 degrees...")
-lift.run_for_degrees(-90)
-zange.run_for_degrees(90)
+lift = Motor('B')
+color = ColorDistanceSensor('A')
+räder = MotorPair('D', 'E')
 
-time.sleep(2)
-print("Running motors for 90 degrees...")
-ml.run_for_degrees(90)
-mr.run_for_degrees(90)
+while True:
+    r, g, b, i = color.get_color_rgb()
 
-print("Done!")
+    if r < 20 and g < 20 and b < 20:
+        fahren.start()  # fährt vorwärts
+        print("Schwarz erkannt: fahre vorwärts")
+    else:
+        fahren.stop()   # stoppt
+        print("Nicht schwarz: stoppt")
+
+    time.sleep(0.1)
