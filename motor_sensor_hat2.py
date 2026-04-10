baab24aa3d18d0f72e97a5d3352d6a58541ea7c0
xy <xy@xy.com>
2026-04-11 01:32:27 +0200

    e

--- motor_sensor_hat2.py
+++ motor_sensor_hat2.py
@@ -4,7 +4,12 @@ import time
 
 from buildhat import Hat, Motor, ColorDistanceSensor, Light 
 from buildhat.exc import DeviceError, MotorError
+from gpiozero import Button
+from signal import pause
+import time
 
+GPIO_BUTTON_ROT = 20 #Pin 38
+GPIO_BUTTON_BLAU = 21 #Pin 40
 H1_RST_GPIO = 4
 H1_BOOT_GPIO = 22
 H2_RST_GPIO = 5
@@ -54,5 +59,26 @@ class Robot():
         color = h1s1.get_color()
         print(f"Color: {color}")
 
+
+def main():
+    # Initialisiert GPIO 6 mit internem Pull-Up
+    button_rot = Button(GPIO_BUTTON_ROT)
+    button_blau = Button(GPIO_BUTTON_BLAU)
+
+    print("Wait on Button blau!")
+    while  not button_blau.is_pressed:
+        time.sleep(0.2)
+
+    if button_rot.is_pressed:
+        print("Button rot ist GEDRÜCKT (Pin ist LOW)")
+    else:
+        print("Button rot ist OFFEN (Pin ist HIGH)")
+    Robot().run()      
+
+    print("Wait on Button rot!")
+    while  not button_rot.is_pressed:
+        time.sleep(0.2)    
+    print("THE END!")  
+
 if __name__ == '__main__':
-    Robot().run()
+  main()
